afc819e43737e7343c24e5c78533c0bbbdb26d77
Regina Guerra <reginaguerra@MacBook-Air-de-Regina.local>
2021-08-18 23:29:20 -0500

    Primer Commit

---
+++ Playground01.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+//Felipe Rodríguez - 18 de Agosto del 2021.
+//Actividad 1. Desarrollo de aplicaciones en plataforma iOS.
+
+var v1 = "esto es Swift"
+
+let c1 = 4
+
+var cadena:String = String() //Variable String.
+
+cadena = "Hola Mundo " + v1 //Concatenación de 2 cadenas.
+
+print("El contenido es: \(cadena)") //Impresión en consola del string "cadena".
+
+print("El valor de la constante es: \(c1)") //Impresión en consola de la constante c1.
+
+
+
